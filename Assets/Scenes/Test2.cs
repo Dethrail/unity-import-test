5610fd17fab1f7f3f90fe9fd310bf2e9e338a812
Maksym Slobodianiuk <max.od.ua@gmail.com>
2021-09-20 16:42:27 +0300

    test

---
+++ Assets/Scenes/Test2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour
+{
+    void Start()
+    {
+        var resource = Resources.Load<GameObject>("GameObject");
+        GameObject.Instantiate(resource);
+
+    }
+}
